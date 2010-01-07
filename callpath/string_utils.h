ccaccd5ab3c0dc7c1755ce289632d67422455fac
Todd Gamblin <tgamblin@login5.intrepid.alcf.anl.gov>
2010-01-06 21:23:40 -0600

    Moved string_utils to callpath library.

--- callpath/string_utils.h
+++ callpath/string_utils.h
@@ -8,13 +8,11 @@ namespace stringutils {
 
   /// Breaks a string into substrings using ANY characters in <delim> as delimiters.
   /// For example, to split a string by commas and whitespace, use ", " for delim.
-  void split(const std::string& str, const std::string& delim, 
-             std::vector<std::string>& parts);
+  void split(const std::string& str, const std::string& delim, std::vector<std::string>& parts);
 
   /// Breaks a string into substrings using only the exact string <delim> as a delimiter.
   /// For example, to split a string by "=>", pass "=>".  
-  void split_str(const std::string& str, const std::string& delim, 
-                 std::vector<std::string>& parts);
+  void split_str(const std::string& str, const std::string& delim, std::vector<std::string>& parts);
 
   /// Trims any characters in <chars> off both ends of a string and returns the result.
   std::string trim(const std::string& str, const std::string chars = " ");
