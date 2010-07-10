815cebd8234b6afaa31b62cd10e3499dc8322747
Todd Gamblin <tgamblin@llnl.gov>
2010-07-10 12:23:56 -0700

    Removed LLNL license from external code.

--- libwavelet/rle.C
+++ libwavelet/rle.C
@@ -1,34 +1,3 @@
-/////////////////////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2010, Lawrence Livermore National Security, LLC.  
-// Produced at the Lawrence Livermore National Laboratory  
-// Written by Todd Gamblin, tgamblin@llnl.gov.
-// LLNL-CODE-417602
-// All rights reserved.  
-// 
-// This file is part of Libra. For details, see http://github.com/tgamblin/libra.
-// Please also read the LICENSE file for further information.
-// 
-// Redistribution and use in source and binary forms, with or without modification, are
-// permitted provided that the following conditions are met:
-// 
-//  * Redistributions of source code must retain the above copyright notice, this list of
-//    conditions and the disclaimer below.
-//  * Redistributions in binary form must reproduce the above copyright notice, this list of
-//    conditions and the disclaimer (as noted below) in the documentation and/or other materials
-//    provided with the distribution.
-//  * Neither the name of the LLNS/LLNL nor the names of its contributors may be used to endorse
-//    or promote products derived from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
-// LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-/////////////////////////////////////////////////////////////////////////////////////////////////
 /*************************************************************************
  * Name:        rle.c
  * Author:      Marcus Geelnard
@@ -425,7 +394,7 @@ static size_t RLE_Parse(unsigned char *in, size_t insize, unsigned char marker,
  * PRE:  Input buffer is valid run-length encoded data, not including 
  *       first byte for marker.
  * 
- * POST: input buffer is rewritten onto the output streamusing the new
+ * POST: input buffer is rewritten onto the output stream using the new
  *       marker provided.  Note that the new marker is not written to the
  *       output stream, just the data.
  * 
@@ -514,7 +483,8 @@ size_t RLE_Merge(unsigned char **bufs, size_t *sizes, size_t num_bufs, unsigned 
     }
     i++;
     inpos = 1;
-  }                                                                                                                                   
+  }
+
   if (count1 == 1) {
     _RLE_WriteNonRep(out, &outpos, new_marker, sym1);
   } else {
