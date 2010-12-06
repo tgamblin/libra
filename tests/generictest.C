846a81839be8075946dfe546d418ab8eeeec3878
Todd Gamblin <tgamblin@llnl.gov>
2010-12-06 12:11:01 -0800

    Adding a test for read_generic and write_generic.

---
+++ tests/generictest.C
@@ -0,0 +1,93 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2010, Lawrence Livermore National Security, LLC.  
+// Produced at the Lawrence Livermore National Laboratory  
+// Written by Todd Gamblin, tgamblin@llnl.gov.
+// LLNL-CODE-417602
+// All rights reserved.  
+// 
+// This file is part of Libra. For details, see http://github.com/tgamblin/libra.
+// Please also read the LICENSE file for further information.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+// 
+//  * Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the disclaimer below.
+//  * Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the disclaimer (as noted below) in the documentation and/or other materials
+//    provided with the distribution.
+//  * Neither the name of the LLNS/LLNL nor the names of its contributors may be used to endorse
+//    or promote products derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// LAWRENCE LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/////////////////////////////////////////////////////////////////////////////////////////////////
+#include <sstream>
+#include <cstring>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#include "io_utils.h"
+using namespace wavelet;
+
+/// This is a test of the variable-length write routines used
+/// to write compressed header files.
+
+
+int main(int argc, char **argv) {
+  ostringstream os;
+  istringstream is;
+
+  bool pass = true;
+  bool verbose = false;
+  for (int i=1; i < argc; i++) {
+    if (!strcmp(argv[i], "-v")) verbose = true;
+  }
+  
+
+  for (size_t i=0; i < 1048576; i+=17) {
+    os.str("");
+    write_generic(os, i);
+
+    is.str(os.str());
+    size_t j = read_generic<size_t>(is);
+
+    if (i != j) {
+      if (verbose) {
+        cout << "expected " << i << " but got " << j << endl;
+      }
+      pass = false;
+    }
+  }
+
+
+  for (double i=0; i < 1048576.0; i+=M_PI) {
+    os.str("");
+    write_generic(os, i);
+
+    is.str(os.str());
+    double j = read_generic<double>(is);
+
+    if (i != j) {
+      if (verbose) {
+        cout << "expected " << i << " but got " << j << endl;
+      }
+      pass = false;
+    }
+  }
+  
+  
+  if (verbose) {
+    cout << (pass ? "PASSED" : "FAILED") << endl;
+  }
+
+  exit(pass ? 0 : 1);
+}
